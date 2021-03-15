b34639ce438b3059981ec887f119fb2c26aeb9e8
awalker <revengeofthepencil@gmail.com>
2021-03-15 11:32:44 -0700

    add meta data browser

--- audioFileUtils.js
+++ audioFileUtils.js
@@ -1,14 +1,17 @@
-// import { getAudioDecoder } from 'audio-file-decoder';
-// import DecodeAudioWasm from 'audio-file-decoder/decode-audio.wasm'; 
 const fs = require('fs');
-// const getAudioDecoder = require('audio-file-decoder')
+const musicMetadata = require('music-metadata');
 
 const processAudioFile = filePath => {
+
     fs.readFile(filePath, function (err, data) {
         if (err) {
            return console.error(err);
         }
-        console.log("Content: " + data.toString());
+
+        musicMetadata.parseFile(filePath).then(metadata => {
+            console.log(`metadata = ${JSON.stringify(metadata)}`);
+        });
+        
      });
 };
 
